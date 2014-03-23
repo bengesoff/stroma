a49466805053c1e600709314a1a74a763c6a477e
Ben Gesoff <ben.gesoff@gmail.com>
2014-03-23 00:04:47 +0000

    changed db.js to use prototype and set up test route for check function

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes');
-var users = require('./routes/user');
+var data = require('./lib/db.js').db;
 
 var app = express();
 
@@ -24,7 +24,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
 app.get('/', routes.index);
-app.get('/users', users.list);
+app.get('/test', function(req, res) {
+    db.check('Ben Gesoff', 2211, function() {
+        console.log('Success');
+    });
+});
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
